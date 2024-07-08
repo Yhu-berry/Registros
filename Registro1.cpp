1955c0749cfd7d5e2a5be9f83eef316e1b89ee45
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-08 10:01:27 -0500

    Registro1.cpp: Uso del bucle for para hallar al que tiene sueldo maximo y sueldo minimo

--- Registro1.cpp
+++ Registro1.cpp
@@ -1,8 +1,9 @@
+//Datos de los empleados de DataTech
 #include <iostream>
 #include <string>
 using namespace std;
 
-struct Empleado {
+struct empleado {
     string nombre;
     char sexo;
     double sueldo;
@@ -12,16 +13,30 @@ int main() {
     int n;
     cin >> n;
 
-    Empleado empleados[n];
+    empleado Empleados[n];
 
     for (int i = 0; i < n; ++i) {
         cout << "Nombre del empleado " << i + 1 << ": ";
-        cin >> empleados[i].nombre;
+        cin >> Empleados[i].nombre;
         cout << "Sexo del empleado (M/F) " << ": ";
-        cin >> empleados[i].sexo;
+        cin >> Empleados[i].sexo;
         cout << "Sueldo del empleado " << ": ";
-        cin >> empleados[i].sueldo;
+        cin >> Empleados[i].sueldo;
     }
 
+    int maySue=0;
+    int meSue=0;
+    //se ha de iterar desde 1 para no comparar el primer elemento consigo mismo
+    for(int i=1;i<n;i++){
+    if(Empleados[maySue].sueldo < Empleados[i].sueldo){
+        maySue = i;
+    }   
+    if(Empleados[meSue].sueldo > Empleados[i].sueldo){
+        meSue= i;
+    }
+    }
+    //mostrar el sueldo mayor y el sueldo menor de los empleados 
+    cout<<Empleados[maySue].sueldo<<endl;
+    cout<<Empleados[meSue].sueldo<<endl;
     return 0;
 }
